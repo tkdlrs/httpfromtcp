246e9b9b4b689b7ae68f03d2615415211da14b17
tkdlrs <tkd.lsanchez@gmail.com>
2025-09-22 18:29:47 -0600

    Makes more updates.

--- internal/request/request.go
+++ internal/request/request.go
@@ -18,12 +18,14 @@ type RequestLine struct {
 	RequestTarget string
 	Method        string
 }
+
 type requestState int
 
 const (
 	requestStateInitialized requestState = iota
 	requestStateDone
 )
+
 const crlf = "\r\n"
 const bufferSize = 8
 
@@ -33,15 +35,13 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 	req := &Request{
 		state: requestStateInitialized,
 	}
-	//
 	for req.state != requestStateDone {
-		// Handle out of buffer space
 		if readToIndex >= len(buf) {
 			newBuf := make([]byte, len(buf)*2)
 			copy(newBuf, buf)
 			buf = newBuf
 		}
-		//
+
 		numBytesRead, err := reader.Read(buf[readToIndex:])
 		if err != nil {
 			if errors.Is(err, io.EOF) {
@@ -51,16 +51,15 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 			return nil, err
 		}
 		readToIndex += numBytesRead
-		//
+
 		numBytesParsed, err := req.parse(buf[:readToIndex])
 		if err != nil {
 			return nil, err
 		}
-		//
+
 		copy(buf, buf[numBytesParsed:])
 		readToIndex -= numBytesParsed
 	}
-	//
 	return req, nil
 }
 
@@ -72,9 +71,8 @@ func parseRequestLine(data []byte) (*RequestLine, int, error) {
 	requestLineText := string(data[:idx])
 	requestLine, err := requestLineFromString(requestLineText)
 	if err != nil {
-		return nil, 0, nil
+		return nil, 0, err
 	}
-	//
 	return requestLine, idx + 2, nil
 }
 
@@ -83,32 +81,30 @@ func requestLineFromString(str string) (*RequestLine, error) {
 	if len(parts) != 3 {
 		return nil, fmt.Errorf("poorly formatted request-line: %s", str)
 	}
-	//
+
 	method := parts[0]
-	// Loop over each chacter and ensure that it's alphabetic
 	for _, c := range method {
 		if c < 'A' || c > 'Z' {
 			return nil, fmt.Errorf("invalid method: %s", method)
 		}
 	}
-	//
+
 	requestTarget := parts[1]
-	//
+
 	versionParts := strings.Split(parts[2], "/")
 	if len(versionParts) != 2 {
-		return nil, fmt.Errorf("malformed start-line %s", str)
+		return nil, fmt.Errorf("malformed start-line: %s", str)
 	}
-	//
+
 	httpPart := versionParts[0]
 	if httpPart != "HTTP" {
-		return nil, fmt.Errorf("unrecognized HTTP-part: %s", httpPart)
+		return nil, fmt.Errorf("unrecognized HTTP-version: %s", httpPart)
 	}
-	//
 	version := versionParts[1]
 	if version != "1.1" {
-		return nil, fmt.Errorf("unrecognixed HTTP-version: %s", version)
+		return nil, fmt.Errorf("unrecognized HTTP-version: %s", version)
 	}
-	//
+
 	return &RequestLine{
 		Method:        method,
 		RequestTarget: requestTarget,
@@ -117,27 +113,23 @@ func requestLineFromString(str string) (*RequestLine, error) {
 }
 
 func (r *Request) parse(data []byte) (int, error) {
-	// Accepts the next slice of bytes that needs to be parsed into the Request struct. But where? the Request struct doesn't have raw data so how
 	switch r.state {
 	case requestStateInitialized:
 		requestLine, n, err := parseRequestLine(data)
-		// Handle errors from parseRequestLine
 		if err != nil {
+			// something actually went wrong
 			return 0, err
 		}
-		// parseRequestLine needs more data
 		if n == 0 {
+			// need more data
 			return 0, nil
 		}
-		// store the requestLineStruct in r.RequestLine
 		r.RequestLine = *requestLine
-		// update r.state
 		r.state = requestStateDone
-		// return num successfully consumed and parsed bytes
 		return n, nil
 	case requestStateDone:
 		return 0, fmt.Errorf("error: trying to read data in a done state")
 	default:
-		return 0, fmt.Errorf("error: unknown state %v", r.state)
+		return 0, fmt.Errorf("unknown state")
 	}
 }
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -8,28 +8,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type chunkReader struct {
-	data            string
-	numBytesPerRead int
-	pos             int
-}
-
-// Read reads up to len(p) or numBytesPerRead bytes from the string per call
-// its useful for simulating reading a variable number of bytes per chunk from a network connection
-func (cr *chunkReader) Read(p []byte) (n int, err error) {
-	if cr.pos >= len(cr.data) {
-		return 0, io.EOF
-	}
-	endIndex := cr.pos + cr.numBytesPerRead
-	if endIndex > len(cr.data) {
-		endIndex = len(cr.data)
-	}
-	n = copy(p, cr.data[cr.pos:endIndex])
-	cr.pos += n
-	//
-	return n, nil
-}
-
 func TestRequestLineParse(t *testing.T) {
 	// Test: Good GET Request line
 	reader := &chunkReader{
@@ -58,7 +36,7 @@ func TestRequestLineParse(t *testing.T) {
 	// Test: Good POST Request with path
 	reader = &chunkReader{
 		data:            "POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
-		numBytesPerRead: 3,
+		numBytesPerRead: 5,
 	}
 	r, err = RequestFromReader(reader)
 	require.NoError(t, err)
@@ -72,7 +50,7 @@ func TestRequestLineParse(t *testing.T) {
 		data:            "/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
 		numBytesPerRead: 3,
 	}
-	_, err = RequestFromReader(reader)
+	r, err = RequestFromReader(reader)
 	require.Error(t, err)
 
 	// Test: Invalid method (out of order) Request line
@@ -80,15 +58,34 @@ func TestRequestLineParse(t *testing.T) {
 		data:            "/coffee POST HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
 		numBytesPerRead: 3,
 	}
-	_, err = RequestFromReader(reader)
+	r, err = RequestFromReader(reader)
 	require.Error(t, err)
 
 	// Test: Invalid version in Request line
 	reader = &chunkReader{
 		data:            "OPTIONS /prime/rib TCP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
-		numBytesPerRead: 3,
+		numBytesPerRead: 50,
 	}
-	_, err = RequestFromReader(reader)
+	r, err = RequestFromReader(reader)
 	require.Error(t, err)
+}
 
+type chunkReader struct {
+	data            string
+	numBytesPerRead int
+	pos             int
+}
+
+func (cr *chunkReader) Read(p []byte) (n int, err error) {
+	if cr.pos >= len(cr.data) {
+		return 0, io.EOF
+	}
+	endIndex := cr.pos + cr.numBytesPerRead
+	if endIndex > len(cr.data) {
+		endIndex = len(cr.data)
+	}
+	n = copy(p, cr.data[cr.pos:endIndex])
+	cr.pos += n
+
+	return n, nil
 }

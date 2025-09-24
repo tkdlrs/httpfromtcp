ea27f459ea203219caaf3351c1d11c5befcae6f7
tkdlrs <tkd.lsanchez@gmail.com>
2025-09-24 16:04:14 -0600

    Go through 'Chapter 4: Request Lines | Lesson 6: Connect the Parsing' in the 'Learn the HTTP Protocol in Go' course on Boot.dev.

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"github.com/tkdlrs/httpfromtcp/internal/request"
 )
 
 const port = ":42069"
@@ -27,44 +26,17 @@ func main() {
 		}
 		fmt.Println("Accepted connection from", conn.RemoteAddr())
 		//
-		linesChan := getLinesChannel(conn)
-		//
-		for line := range linesChan {
-			fmt.Println(line)
+		linesChan, err := request.RequestFromReader(conn)
+		if err != nil {
+			fmt.Errorf("%v", err)
+			return
 		}
-		fmt.Println("Connection to ", conn.RemoteAddr(), "closed")
-	}
-}
+		printTemplate := fmt.Sprintf(`Request line:
+- Method: %s
+- Target: %s
+- Version: %s
+`, linesChan.RequestLine.Method, linesChan.RequestLine.RequestTarget, linesChan.RequestLine.HttpVersion)
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string)
-	go func() {
-		defer f.Close()
-		defer close(lines)
-		currentLineContents := ""
-		for {
-			buffer := make([]byte, 8)
-			n, err := f.Read(buffer)
-			if err != nil {
-				if currentLineContents != "" {
-					lines <- currentLineContents
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				break
-			}
-			//
-			str := string(buffer[:n])
-			parts := strings.Split(str, "\n")
-			//
-			for i := 0; i < len(parts)-1; i++ {
-				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
-				currentLineContents = ""
-			}
-			currentLineContents += parts[len(parts)-1]
-		}
-	}()
-	return lines
+		fmt.Printf(printTemplate)
+	}
 }

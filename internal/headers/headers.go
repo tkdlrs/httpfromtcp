55bf134b9fc95d1fc92d5bc9c8c183ba047fa2d7
tkdlrs <tkd.lsanchez@gmail.com>
2025-09-27 12:38:45 -0600

    Completes 'Chapter 5: HTTP Headers | Lesson 3: Multiple Values' of the 'Learn the HTTP Protocol in Go' course on Boot.dev.

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,7 +3,6 @@ package headers
 import (
 	"bytes"
 	"fmt"
-	"slices"
 	"strings"
 )
 
@@ -41,34 +40,35 @@ func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	if !validTokens([]byte(key)) {
 		return 0, false, fmt.Errorf("invalid header token found: %s", key)
 	}
+	//
 	h.Set(key, string(value))
 	return idx + 2, false, nil
 }
 
 func (h Headers) Set(key, value string) {
 	key = strings.ToLower(key)
+	v, ok := h[key]
+	if ok {
+		value = strings.Join([]string{
+			v,
+			value,
+		}, ", ")
+	}
 	h[key] = value
 }
 
-var tokenChars = []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '^', '_', '`', '|', '~'}
+var tokenChars = []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
 
 // validTokens checks if the data contains only valid tokens
 // or characters that are allowed in a token
 func validTokens(data []byte) bool {
 	for _, c := range data {
-		if !isTokenChar(c) {
+		if !(c >= 'A' && c <= 'Z' ||
+			c >= 'a' && c <= 'z' ||
+			c >= '0' && c <= '9' ||
+			c == '-') {
 			return false
 		}
 	}
 	return true
-}
-
-func isTokenChar(c byte) bool {
-	if c >= 'A' && c <= 'Z' ||
-		c >= 'a' && c <= 'z' ||
-		c >= '0' && c <= '9' {
-		return true
-	}
-	//
-	return slices.Contains(tokenChars, c)
 }

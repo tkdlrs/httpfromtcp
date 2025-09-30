790fb98a7295eeb452435b919596ffeeb5bca394
tkdlrs <tkd.lsanchez@gmail.com>
2025-09-29 18:54:47 -0600

    Completes 'Chapter 5: HTTP Headers | Lesson 5: Live Headers' without needing to look at the solution. Was a super easy lesson.

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -34,5 +34,9 @@ func main() {
 		fmt.Printf("- Method: %s\n", req.RequestLine.Method)
 		fmt.Printf("- Target: %s\n", req.RequestLine.RequestTarget)
 		fmt.Printf("- Version: %s\n", req.RequestLine.HttpVersion)
+		fmt.Println("Headers:")
+		for key, val := range req.Headers {
+			fmt.Printf("- %s: %s\n", key, val)
+		}
 	}
 }

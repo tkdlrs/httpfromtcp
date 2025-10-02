a4aad2445b8b9e9700c72e1b73517f2dcf074057
tkdlrs <tkd.lsanchez@gmail.com>
2025-10-01 18:40:32 -0600

    Gets through 'Chapter 7: HTTP Responses | Lesson 1: Server' of the 'Learn the HTTP Protocol in Go' course on Boot.dev. Needed help getting through it.

---
+++ internal/server/server.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync/atomic"
+)
+
+// Contains the state of the server. Server is an HTTP 1.1 server
+type Server struct {
+	listener net.Listener
+	closed   atomic.Bool
+}
+
+// Creates a net.Listener and returns a new Server instance. Starts listening for requests inside a goroutine.
+func Serve(port int) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+	s := &Server{
+		listener: listener,
+	}
+	go s.listen()
+	return s, nil
+}
+
+// Closes the listener and the server
+func (s *Server) Close() error {
+	s.closed.Store(true)
+	if s.listener != nil {
+		return s.listener.Close()
+	}
+	return nil
+}
+
+// Uses a loop to .Accept new connection as they come in, and handles each one in a new fotoutien.
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if s.closed.Load() {
+				return
+			}
+			log.Printf("Error accepting connection: %v", err)
+			continue
+		}
+		go s.handle(conn)
+	}
+}
+
+// Handles a single connection by writing the following response and then closing the connection.
+func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+	response := "HTTP/1.1 200 OK\r\n" + // Status line
+		"Content-Type: text/plain\r\n" + // Example header
+		"Content-Length: 13\r\n" + // Content length header
+		"\r\n" + // Blank line to separate headers from the body
+		"Hello World!\n" //Body
+	conn.Write([]byte(response))
+	return
+}
